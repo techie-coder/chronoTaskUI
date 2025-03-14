ea46d89399f357440c4a18f226c5745d909301e0
Saswata Biswas <saswata.biswas20@gmail.com>
2025-03-14 18:52:05 +0530

    Added z-indexing and mobile-mode

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,23 +1,53 @@
 import logo from "../assets/chronoIcon.png";
 import sticky from "../assets/sticky.png";
+import check from "../assets/check.png";
+import tasks from "../assets/tasks.png";
+import meetings from "../assets/meetings.png";
+import stopwatch from "../assets/stopwatch.png";
+import gmail from "../assets/gmail.png";
+import slack from "../assets/slack.png";
+import gcalendar from "../assets/gcalendar.png";
+
 
 const LandingPage = () => {
     return (
         <>
             <div className="relative flex justify-center items-center h-[90vh]">
-                <div className="absolute -z-10 inset-0 left-5 right-5 h-[95%] bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:8px_8px] rounded-3xl border-2 border-[#e5e7eb]">
-                    <img className="rotate-5 drop-shadow-lg" src={sticky} height={300} width={300}></img>
+                <div className="absolute -z-10 overflow-hidden inset-0 left-5 right-5 h-[95%] bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:8px_8px] rounded-3xl border-2 border-[#e5e7eb]">
+                    <div className="absolute top-42 -left-20 rotate-15 z-2 bg-white/20 backdrop-blur-[1px] w-[300px] h-[200px] border border-white/20 rounded-xl shadow-lg not-md:hidden"></div>
+                    <div className="absolute z-5 top-55 left-20 bg-white h-[100px] w-[100px] flex items-center justify-center rounded-xl shadow-lg not-md:hidden">
+                        <img src={check} height={45} width={45} className="z-6 drop-shadow-xl"></img>
+                    </div>
+                    <img className="relative z-0 rotate-5 drop-shadow-lg not-md:hidden" src={sticky} height={280} width={280}></img>
+                    <img src={tasks} height={280} width={280} className="absolute -bottom-5 left-15 -rotate-5 drop-shadow-md not-md:hidden"></img>
+                    <img src={meetings} height={280} width={280} className="absolute top-15 -right-20 rotate-10 drop-shadow-lg not-md:hidden"></img>
+                    <div className="absolute top-40 -right-60 rotate-20 z-2 bg-white/20 backdrop-blur-[1px] w-[300px] h-[250px] border border-white/20 rounded-xl shadow-lg not-md:hidden"></div>
+                    <div className="absolute z-2 top-22 right-45 bg-white h-[100px] w-[100px] rounded-xl flex justify-center items-center shadow-xl -rotate-5 not-md:hidden">
+                        <img src={stopwatch} height={60} width={60} className="z-5 drop-shadow-lg"></img>
+                    </div>
+                    <img src={tasks} height={280} width={280} className="absolute z-0 -bottom-5 right-15 rotate-5 drop-shadow-md not-md:hidden"></img>
+                    <div className="absolute z-5 -bottom-22 right-17 bg-[#e5e7eb] h-[280px] w-[270px] rotate-5 poppins-medium not-md:hidden">100+ integrations</div>
+                    <div className="absolute z-6 bottom-8 right-55 -rotate-5 bg-white h-[90px] w-[90px] rounded-xl shadow-lg flex justify-center items-center not-md:hidden">
+                        <img src={gmail} height={50} width={50} className=""></img>
+                    </div>
+                    <div className="absolute z-7 bottom-12 right-35 bg-white h-[90px] w-[90px] rounded-xl drop-shadow-2xl flex justify-center items-center not-md:hidden">
+                        <img src={slack} height={50} width={50} className=""></img>
+                    </div>
+                    <div className="absolute z-6 bottom-6 right-20 rotate-7 bg-white h-[90px] w-[90px] rounded-xl drop-shadow-2xl flex justify-center items-center not-md:hidden">
+                        <img src={gcalendar} height={50} width={50} className=""></img>
+                    </div>
                 </div>
                 <div className="flex flex-col justify-center items-center space-y-4">
-                    <img src={logo} height={100} width={100} className="shadow-t-md shadow-2xl rounded-2xl"></img>
+                    <img src={logo} height={100} width={100} className="shadow-t-md drop-shadow-2xl rounded-2xl"></img>
                     <div className="text-center">
-                        <h1 className="text-7xl font-geist">Think, plan, and track</h1>
-                        <h2 className="text-7xl font-geist text-[#A6A6A4]">all in one place</h2>
+                        <h1 className="text-7xl font-geist not-md:text-3xl">Think, plan, and track</h1>
+                        <h2 className="text-7xl font-geist text-[#A6A6A4] not-md:text-3xl">all in one place</h2>
                     </div>
-                    <h3 className="text-lg font-geist">Efficiently manage your tasks and boost productivity.</h3>
+                    <h3 className="text-lg font-geist not-md:text-sm">Efficiently manage your tasks and boost productivity.</h3>
                     <button className="bg-blue-500 text-white rounded-[0.7rem] px-8 py-[12px]">Get free demo</button>
                 </div>
             </div>
+            <div className="w-screen text-center">Coded by <a href="https://github.com/techie-coder" className="underline">Saswata</a>| <a href="https://dribbble.com/shots/25000009-ChronoTask-Landing-Page" className="underline">Original Design From Dribble</a></div>
         </>
     )
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,16 @@
+import { useState } from "react";
 import icon from "../assets/chronoIcon.png";
+import menuIcon from "../assets/menu.png"
+import crossIcon from "../assets/cross.png";
 
 const NavBar = () => {
+
+    const [menu, setMenu] = useState(false);
+
+    const handleClick = () => {
+        setMenu(!menu);
+    }
+
     return (
         <>
             <nav className="w-full flex justify-between p-5">
@@ -10,7 +20,7 @@ const NavBar = () => {
                         <span className="text-2xl font-geist font-bold">ChronoTask</span>
                     </a>
                 </span>
-                <span className="flex items-center justify-center">
+                <span className="flex items-center justify-center not-md:hidden">
                     <ul className="flex flex-row gap-7 text-sm font-geist">
                         <li><a href="#">Features</a></li>
                         <li><a href="#">Solutions</a></li>
@@ -18,7 +28,7 @@ const NavBar = () => {
                         <li><a href="#">Pricing</a></li>
                     </ul>
                 </span>
-                <span className="flex items-center justify-center gap-7">
+                <span className="flex items-center justify-center gap-7 not-md:hidden">
                     <button className="text-sm font-geist">
                         Sign In
                     </button>
@@ -30,7 +40,18 @@ const NavBar = () => {
                         </div>
                     </button>
                 </span>
+                <button className="md:hidden" onClick={handleClick}>
+                    <img src={menu ? crossIcon : menuIcon} height={30} width={30}></img>
+                </button>
             </nav >
+            {menu ? <div className="absolute bg-white/20 backdrop-blur-md z-1  h-screen w-screen text-3xl font-geist flex flex-col justify-center items-center space-y-4">
+                <ul>Features</ul>
+                <ul>Solutions</ul>
+                <ul>Resources</ul>
+                <ul>Pricing</ul>
+                <ul>Sign In</ul>
+                <ul>Get Demo</ul>
+            </div> : <></>}
         </>
     )
 }
